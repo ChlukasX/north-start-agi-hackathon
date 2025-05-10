54f9413d506ec4b1d944481a807daefa1c7d8f74
Chlukas <lukasnackmayr@proton.me>
2025-05-10 13:00:26 +0200

    added more basic agent example code

--- agents/agent_example.py
+++ agents/agent_example.py
@@ -1,8 +1,42 @@
+from langchain_openai import ChatOpenAI
 from base_agent import BaseAgent
+from langchain_core.messages import HumanMessage, SystemMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
 
 
 class ExampleAgent(BaseAgent):
     def __init__(self):
         super().__init__()
 
-        self._initialize_maps_api()
+    def get_tools(self):
+        base_tools = super().get_tools()
+
+        # Make more tools from based tools specific to the agent
+
+    def llm(self, query):
+        memory = MemorySaver()
+
+        # Create a system message for the LLM
+        system_message = SystemMessage(content="You are a helpful assistant.")
+
+        # Initialize the LLM
+        llm = ChatOpenAI()
+
+        agent_executor = create_react_agent(
+            llm,
+            self.get_tools(),
+            checkpoint=memory,
+        )
+
+        config = {"configurable": {"thread_id": "example_agent"}}
+
+        return agent_executor.stream(
+            {"messages": [system_message, HumanMessage(content=query)]},
+            config,
+            stream_mode="values",
+        )
+
+    def llm_run(self, query):
+        for step in self.llm(query):
+            step["messages"][-1].pretty_print()
